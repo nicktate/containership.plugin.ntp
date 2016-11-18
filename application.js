7dc65f201e0e3aea3b2bb038f31f15f7774f0cb5
Nick Tate <ntate22@gmail.com>
2016-11-18 08:45:11 -0500

    manually start ntp on leader nodes as self restarting with failure li… (#5)
    * manually start ntp on leader nodes as self restarting with failure limit docker process
    
    * only start ntp on leader node if it is not already running
    
    * fix container name

--- application.js
+++ application.js
@@ -3,6 +3,9 @@
 const _ = require('lodash');
 const ContainershipPlugin = require('containership.plugin');
 
+const Docker = require("dockerode");
+const docker = new Docker({socketPath: "/var/run/docker.sock"});
+
 module.exports = new ContainershipPlugin({
     name: 'ntp',
     type: 'core',
@@ -47,6 +50,66 @@ module.exports = new ContainershipPlugin({
                 }
             });
         };
+
+        const addNtpToLeaderNode = (nodeId) => {
+            docker.listContainers((err, containers) => {
+                if (err) {
+                    return core.loggers[applicationName].log('error', `Failed to list existing containers on leader node[${nodeId}]\n${err.message}`);
+                }
+
+                let isNtpRunning = false;
+
+                _.forEach(containers, (container) => {
+                    if (container.Names[0].slice(1) === 'containership-ntp') {
+                        isNtpRunning = true;
+                        return false; // break out of forEach
+                    }
+                });
+
+                if (isNtpRunning) {
+                    return;
+                }
+
+                docker.pull('containership/ntp:latest', (err, stream) => {
+                    if (err) {
+                        return core.loggers[applicationName].log('error', `Failed to pull containership/ntp on leader node[${nodeId}]\n${err.message}`);
+                    }
+
+                    docker.modem.followProgress(stream, onFinished);
+                    function onFinished(err, output) {
+                        if (err) {
+                            return core.loggers[applicationName].log('error', `Failed to pull containership/ntp on leader node[${nodeId}]\n${err.message}`);
+                        }
+
+                        core.loggers[applicationName].log('verbose', `Starting containership/ntp on leader node[${nodeId}]`);
+                        docker.run('containership/ntp:latest', [], process.stdout, {
+                            name: 'containership-ntp',
+                            Binds: ["/var/run/docker.sock:/var/run/docker.sock"],
+                            HostConfig: {
+                                Privileged: true,
+                                RestartPolicy: {
+                                    Name: "on-failure",
+                                    MaximumRetryCount: 5
+                                },
+                                CpuShares: Math.floor(0.1 * 1024),
+                                Memory: 16 * 1024 * 1024 // 16MB,
+                            }
+                        }, (err, data, container) => {
+                            if(err) {
+                                return core.loggers[applicationName].log('error', `Failed to run containership/ntp on leader node[${nodeId}]\n${err.message}`);
+                            }
+                        });
+                    }
+                });
+            });
+        };
+
+        // launch NTP on leader nodes
+        if(core.options.mode == 'leader') {
+            const attributes = core.cluster.legiond.get_attributes();
+            const nodeId = attributes.id;
+            addNtpToLeaderNode(nodeId);
+        }
 
         if(core.cluster.praetor.is_controlling_leader()) {
             addApplication();
